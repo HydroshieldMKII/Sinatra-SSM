0831d31757f379f2536378bb8522048428c0e90c
HydroshieldMKII <114195376+HydroshieldMKII@users.noreply.github.com>
2024-04-28 00:20:54 -0400

    Refactor session management and add password hashing options in SimpleSessionManager.rb

--- ssm/SimpleSessionManager.rb
+++ ssm/SimpleSessionManager.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'rack/session/cookie' #Session control
 require 'dotenv/load' #Load .env file
+require 'bcrypt' #For advanced password hashing
 require "openssl" #For sha256
 require 'logger' #For logging
 require 'json' #For json parsing
@@ -9,10 +10,16 @@ require 'json' #For json parsing
 COOKIE_NAME = ENV['COOKIE_NAME'] || "rack.session" #Name of the session cookie in the browser
 SESSION_KEY = ENV['SESSION_KEY'] || "username" #Unique key to store in the session that identifies the user
 SESSION_SECRET = ENV['SESSION_SECRET'] #At least 64 characters
-SESSION_EXPIRE = ENV['SESSION_EXPIRE'].to_i #In seconds
+SESSION_EXPIRE = ENV['SESSION_EXPIRE'].to_i || 259200 #In seconds
 
-#Sha key for the password hashing
-SHA_KEY = ENV['SHA_KEY'] #SHA key for password hashing
+#Strict password settings
+STRICT_PASSWORD = ENV['STRICT_PASSWORD'] || false #Use bcrypt for password hashing, with strict password requirements
+PWD_MIN_PASSWORD_LENGTH = ENV['PWD_MIN_PASSWORD_LENGTH'].to_i || 8 #Minimum password length
+PWD_MIN_SPECIAL_CHARS = ENV['PWD_MIN_SPECIAL_CHARS'].to_i || 1 #Minimum special characters
+PWD_MIN_NUMBERS = ENV['PWD_MIN_NUMBERS'].to_i || 1 #Minimum numbers
+
+#SHA settings
+SHA_KEY = ENV['SHA_KEY'] #Secret key for sha256 hashing your passwords
 
 #Login settings
 LOGIN_URL = ENV['LOGIN_PATH'] || "/login" #Path to the login page
@@ -27,7 +34,7 @@ use Rack::Session::Cookie,  :key => COOKIE_NAME,
 
 #Logging settings
 LOGGING = false
-LOG_FILE = ENV['LOG_PATH'] #Path to the log file
+LOG_FILE = ENV['LOG_PATH'] || "log.txt" #Path to the log file
 
 module Sinatra
   module SSM
@@ -51,13 +58,14 @@ module Sinatra
         def login!(value)
             return true unless session[SESSION_KEY].nil?
 
+            #no session, check for basic auth in the request
             raise "No value provided to set session" if value.nil?
 
             #check if basic auth is provided
             auth = Rack::Auth::Basic::Request.new(request.env) 
             return false unless auth.provided? && auth.basic? && auth.credentials
 
-            #Check if the credentials match
+            #Check if the credentials are correct
             username, password = auth.credentials
             userInfo = authenticate(username, password)
 
@@ -101,20 +109,26 @@ module Sinatra
             end
         end
 
-        def add_user!(user_data)
+        def add_user!(user_data) #=> { 'username' => '...', 'password' => '...' }
             raise "No data provided" if user_data.nil?
             raise "The data provided is not a hash" unless user_data.is_a? Hash
-            raise "No username provided" if user_data['username'].nil?
-            raise "No password provided" if user_data['password'].nil?
+
+            username = user_data['username'] || raise "No username provided"
+            password = user_data['password'] || raise "No password provided"
 
             begin
-                #Check if the user already exists
                 users = JSON.parse(File.read(USERS_LOCATION))
-                raise "A user with the same username already exists" if users.find { |user| user['username'] == user_data['username'] }
+                if STRICT_PASSWORD
+                    raise "Password does not meet the requirements" unless password.length >= PWD_MIN_PASSWORD_LENGTH
+                    raise "Password does not meet the requirements" unless password.count("0-9") >= PWD_MIN_NUMBERS
+                    raise "Password does not meet the requirements" unless password.count("!@#$%^&*") >= PWD_MIN_SPECIAL_CHARS
+                    s_password = bcrypt_hash(password)
+                else
+                    s_password = sha256(password)
+                end
 
-                #Change the password to sha256
-                user_data['password'] = sha256(user_data['password'])
-        
+                user_data['password'] = s_password
+
                 users << user_data
                 File.write(USERS_LOCATION, users.to_json)
                 return true
@@ -131,6 +145,16 @@ module Sinatra
             $logger.info(message) if type == "info"
         end
 
+        # Hashes a string using bcrypt
+        def bcrypt_hash(password)
+            BCrypt::Password.create(password)
+        end
+
+        # Verifies a password against a bcrypt hash
+        def bcrypt_verify(password, hash)
+            BCrypt::Password.new(hash) == password
+        end
+
         #Hashes a string using sha256
         def sha256(value)
             nil if value.nil? || value.empty?
@@ -141,7 +165,11 @@ module Sinatra
         def authenticate(username, password)
             begin
                 users = JSON.parse(File.read(USERS_LOCATION))
-                return users.find { |user| user['username'] == username && user['password'] == sha256(password)}
+                if STRICT_PASSWORD
+                    return users.find { |user| user['username'] == username && bcrypt_verify(password, user['password']) }
+                else
+                    return users.find { |user| user['username'] == username && user['password'] == sha256(password)}
+                end
             rescue Exception => e
                 raise e
             end
@@ -152,15 +180,21 @@ module Sinatra
         app.helpers SSM::Helpers
 
         #Verification of core settings
-        SHA_KEY || raise("SHA_KEY not found in .env file")
+        if STRICT_PASSWORD
+            PWD_MIN_PASSWORD_LENGTH || raise("PWD_MIN_PASSWORD_LENGTH not found in .env file")
+            PWD_MIN_SPECIAL_CHARS || raise("PWD_MIN_SPECIAL_CHARS not found in .env file")
+            PWD_MIN_NUMBERS || raise("PWD_MIN_NUMBERS not found in .env file")
+        else
+            SHA_KEY || raise("SHA_KEY not found in .env file")
+        end
+
         SESSION_SECRET || raise("SESSION_SECRET not found in .env file")
-        SESSION_EXPIRE || raise("SESSION_EXPIRE not found in .env file")
-        USERS_LOCATION || raise("USERS_LOCATION not found in .env file")
+
 
         #Check if the users file exists and it's json
         begin
             raise "Users file not found at '#{USERS_LOCATION}'" unless File.exist?(USERS_LOCATION)
-            JSON.parse(File.read(USERS_LOCATION))
+            JSON.parse(File.read(USERS_LOCATION)) #Check if the file is json
         rescue Exception => e
             raise e
         end
